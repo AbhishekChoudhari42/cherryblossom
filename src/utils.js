0ecca4545c004a5f54837992c1972cb226503384
AbhishekChoudhari42 <choudhariabhishek93@gmail.com>
2024-06-15 16:12:55 +0530

    drawFlowers

--- src/utils.js
+++ src/utils.js
@@ -21,6 +21,8 @@ function random(min,max){
 function drawBranch(ctx,x1,y1,angle,depth){
     const BRANCH_LENGTH = Math.random()*depth*9
 
+    drawFlowers(ctx,x1,y1,depth)
+
     if(depth!=0){
         const x2 = x1 + cos(angle)*BRANCH_LENGTH
         const y2 = y1 + sin(angle)*BRANCH_LENGTH
@@ -45,4 +47,26 @@ function drawLine(ctx,x1,y1,x2,y2,depth){
     ctx.lineTo(x2,y2)
     ctx.stroke()
     ctx.closePath()
+}
+
+function drawFlowers(ctx,x1,y1,depth){
+    
+    const r = Math.random()
+
+    if(depth == 0){
+        ctx.beginPath()
+        ctx.fillStyle = r < 0.25 ? "f3ff" : r < 0.8 ? "#d0da" : "#e0ea";
+        ctx.arc(x1,y1,r*5 + 1,0,2*Math.PI)
+        ctx.fill()
+        ctx.closePath()
+        return
+    }
+    
+    if(depth < 6){
+        ctx.beginPath()
+        ctx.fillStyle = "#3f35"
+        ctx.arc(x1,y1,r*3 + 1,0,2*Math.PI)
+        ctx.fill()
+        ctx.closePath()
+    }
 }
