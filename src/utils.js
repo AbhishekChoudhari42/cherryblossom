b0bafa55e23ca2fec0275c6d9b5d8581159e8e2f
AbhishekChoudhari42 <choudhariabhishek93@gmail.com>
2024-06-15 16:37:41 +0530

    extra trees

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,5 @@
 export function drawTree(ctx,width,height){
     drawBranch(ctx,width/2,height - 50,-90,13)
-    drawBranch(ctx,width/2 + 200,height - 50,-90,13)
-    drawBranch(ctx,width/2 - 200,height - 50,-90,13)
 } 
 
 function deg_to_rad(angle){
